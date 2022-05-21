0850a84c6a7e230b5473ff1919300989a3b9c623
Tomáš Martykán <t.martykan@gmail.com>
2022-05-21 07:46:45 +0200

    CloudKit sync improve

--- LemuQR/Managers/CoreDataManager.swift
+++ LemuQR/Managers/CoreDataManager.swift
@@ -19,14 +19,16 @@ class CoreDataManager {
         persistentContainer = NSPersistentCloudKitContainer(name: "DataModel")
         if inMemory,
            let storeDescription = persistentContainer.persistentStoreDescriptions.first {
+            // In memory store for testing
             storeDescription.url = URL(fileURLWithPath: "/dev/null")
         } else {
+            // Use a store inside App Group to share data with widget
             let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.test.lemuqr")!
             let storeURL = containerURL.appendingPathComponent("DataModel.sqlite")
             let storeDescription = persistentContainer.persistentStoreDescriptions.first
             storeDescription?.url = storeURL
         }
-        
+        persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
         persistentContainer.loadPersistentStores { _, error in
             if let error = error as NSError? {
                 fatalError("Unable to configure Core Data Store: \(error), \(error.userInfo)")
